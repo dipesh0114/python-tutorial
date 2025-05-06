fda907167c019a74c61e64997098ab2da83d5aba
Dipesh <dipeshpatro@gmail.com>
2025-05-06 22:50:52 +0530

    sa

---
+++ datatype.py
@@ -0,0 +1,18 @@
+"""
+datatypes 
+
+1. number
+2.string
+3. list  - []
+4. tuple - ()
+5. dictionary - {}
+6. set - set('abc), {'a','b','c'}
+7. file 
+8. boolean
+9. none
+10. functions, modules, classes
+
+advanced - decorators, generators, iterators, metaProgramming
+
+"""
+# dir(username)  helps in python 
