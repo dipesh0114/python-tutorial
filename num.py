d39b9c96745894aa07e1be65482d57820a9ac96d
Dipesh <dipeshpatro@gmail.com>
2025-05-10 04:40:24 +0530

    a

--- num.py
+++ num.py
@@ -1,3 +1,20 @@
 x = 2
 y=3 
 z =4
+
+print((x+y)*z)
+
+operatorOverloading = "chai" + "code"
+print(operatorOverloading)
+
+print(z**5)
+
+print(2 ** 1000)
+
+repr('chai')  # "'chai'"
+str('chai')   # 'chai'
+print('chai') #  chai
+
+
+# true = 1, false = 0
+
