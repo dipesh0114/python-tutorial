07a05c9ad4411ee79868be0a94f33632f532e2ef
Dipesh <dipeshpatro@gmail.com>
2025-06-04 04:15:48 +0530

    as

--- Dict.py
+++ Dict.py
@@ -18,3 +18,20 @@ for key, value in chai_types.items():
 chai_types["earl grey"] = "Citrus"
 print(chai_types)   
 
+tea_shop = {
+    "chai" : {"masala":"spicy", "ginger": "zesty"},
+    "Tea": {"green":"mild", "black" :"strong"}
+}
+
+print(tea_shop['chai'])
+print(tea_shop['chai']['ginger'])
+
+squared_num = {x:x**2 for x in range(6)}
+print(squared_num)
+
+keys = ["msala", "ginger", "lemon"]
+default_value = "delicious"
+
+new_dict = dict.fromkeys(keys, default_value)
+print(new_dict)
+
