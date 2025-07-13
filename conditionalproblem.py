4060608b6db2d4ad56c4175a0125165ecea55222
Dipesh <dipeshpatro@gmail.com>
2025-07-14 00:58:49 +0530

    as

---
+++ conditionalproblem.py
@@ -0,0 +1,11 @@
+age = int(input("provide a age: "))
+
+if age < 13:
+    print("child")
+
+elif age > 13 and age < 20:
+    print("teenager")
+
+else:
+    print("adult")
+
---
+++ movieticket.py
@@ -0,0 +1,9 @@
+age = int(input("enter your age : "))
+day = input("enter day : ")
+
+price = 12 if age >=18 else 8
+
+if day == "wednesday" or day== "wed":
+    price -= 2
+
+print("your ticket price is $", price)
