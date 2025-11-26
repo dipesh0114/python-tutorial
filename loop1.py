ce3bccd8d437c7038ffdbe26899bded61832a8a2
Dipesh <dipeshpatro@gmail.com>
2025-11-26 23:11:01 +0530

    as

--- loop1.py
+++ loop1.py
@@ -1,7 +1,7 @@
 numbers = [1, -2, 3, -4,5, -6, 7 , -8, 9,10]
 positive_count = 0
 
-for num in numbers:
-    if num > 0:
+for i in numbers:
+    if i > 0:
         positive_count += 1
 print("the number of positive numbers in the list: ", positive_count)        
